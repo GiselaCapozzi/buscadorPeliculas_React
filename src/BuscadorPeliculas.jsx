0aae8d9e9b8d9887ad0ba76c3039e7bbd780ff79
GiselaCapozzi <gisecapozzi@gmail.com>
2023-07-28 01:01:38 +0200

    Agregando Navbar

--- src/BuscadorPeliculas.jsx
+++ src/BuscadorPeliculas.jsx
@@ -1,11 +1,15 @@
 import { Buscador } from "./components/Buscador";
 import { Routes, Route } from 'react-router-dom';
 import { DetailsPeliculas } from "./components/DetailsPeliculas";
+import { Navbar } from "./components/Navbar";
+import { Home } from "./components/Home";
 
 export const BuscadorPeliculas = () => {
   return (
     <div className="container">
+      <Navbar />
       <Routes>
+        <Route path="/" element={<Home />}/>
         <Route path="/buscador" element={<Buscador />} />
         <Route path='/details' element={<DetailsPeliculas />}/>
       </Routes>
